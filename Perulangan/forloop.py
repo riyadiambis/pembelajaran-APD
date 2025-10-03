7c298041826f25e108daa9ef7c7dc8afd98343cc
riyadiambis <rahmatriyadiambis@gmail.com>
2025-10-03 11:04:46 +0800

    materi loop

---
+++ Perulangan/forloop.py
@@ -0,0 +1,14 @@
+laporan_keuangan = [
+2500000, 5000000, -1000000, -2500000, 5000000, 10000000,
+-5000000, 7500000, 10000000, -1500000, 25000000, -2500000
+]
+
+total_pengeluaran, total_pemasukan = 0, 0
+for dana in laporan_keuangan:
+    if dana > 0:
+        total_pemasukan += dana
+    else:
+        total_pengeluaran += dana
+total_pengeluaran *= -1
+print(total_pengeluaran) 
+print(total_pemasukan)
---
+++ Perulangan/whileloop.py
@@ -0,0 +1,30 @@
+# # Tagihan
+
+# tagihan = [50000, 75000, -15000, 125000, 300000]
+# i = 0
+# total_tagihan = len(tagihan)
+# harus_dibayar = 0
+
+# while i < total_tagihan:
+#     if tagihan [i] < 0:
+#         i += 1
+#         continue
+#     harus_dibayar += tagihan[i]
+#     i += 1
+
+# print(harus_dibayar)
+
+
+
+# Jawab Gak? Hey
+while True :
+    respon = input('Pilihan Y atau T : ').lower().strip()
+    if respon in ['ya', 'y', 'tidak', 't']:
+        break
+    print('Input yang bener donk jangan ngasal')
+
+print(respon)
+if respon in ['ya', 'y']:
+    print('y')
+else :
+    print('Ini yang salah')
